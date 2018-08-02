6b9a4c05660a8f44d7c017eb6ccab711ea938756
kishlaya <kishlaya.j@gmail.com>
2018-08-03 00:17:31 +0530

    First cut

---
+++ Test.elm
@@ -0,0 +1,70 @@
+module Test
+  exposing (..)
+
+import Wrapper
+import Html exposing (..)
+import Http
+import Task
+import Json.Decode
+import Json.Encode exposing (Value)
+import Json.Helpers exposing (..)
+
+main = 
+  Html.program
+    { view = view
+    , init = init
+    , update = update
+    , subscriptions = \_ -> Sub.none
+    }
+
+type alias Model = Data
+
+type Msg
+  = WrapperMsg (Wrapper.Msg Data)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    WrapperMsg (Wrapper.HttpResult (Ok result)) -> result ! []
+    WrapperMsg (Wrapper.HttpResult (Err err)) -> model ! []
+
+init : (Model, Cmd Msg)
+init = (emptyData, load)
+
+view : Model -> Html Msg
+view model = 
+  div []
+    [ h1 [] [ text model.title ]
+    , p [] [ text model.body ]
+    ]
+
+load : Cmd Msg
+load = 
+  let
+    url = "https://jsonplaceholder.typicode.com/posts/1"
+    request = Http.get url jsonDecData
+  in
+    Wrapper.send WrapperMsg request
+
+type alias Data =
+  { userId : Int
+  , id : Int
+  , title : String
+  , body : String
+  }
+
+emptyData : Data
+emptyData = 
+  { userId = 0
+  , id = 0
+  , title = "Blank title"
+  , body = "Blank body"
+  }
+
+jsonDecData : Json.Decode.Decoder ( Data )
+jsonDecData =
+  ("userId" := Json.Decode.int) >>= \puserId ->
+  ("id" := Json.Decode.int) >>= \pid ->
+  ("title" := Json.Decode.string) >>= \ptitle ->
+  ("body" := Json.Decode.string) >>= \pbody ->
+  Json.Decode.succeed {userId = puserId, id = pid, title = ptitle, body = pbody}
--- Wrapper.elm
+++ Wrapper.elm
@@ -8,16 +8,16 @@ type alias Model =
   { loading: Int
   }
 
-type Msg msg a
-  = SendRequest (Result Http.Error a -> msg) (Result Http.Error a)
+type Msg a
+  = HttpResult (Result Http.Error a)
 
-update : (Msg msg a) -> Model -> (Model, Cmd msg)
+update : (Msg a) -> Model -> (Model, Cmd msg)
 update msg model =
   case msg of
-    SendRequest externalMsg result -> (model, pure <| externalMsg result)
+    HttpResult result -> (model, Cmd.none)
 
-send : ((Msg msg a) -> msg) -> (Result Http.Error a -> msg) -> (Http.Request a) -> Cmd msg
-send wrapper externalMsg request = Cmd.map wrapper <| Http.send (SendRequest externalMsg) request
+send : ((Msg a) -> msg) -> (Http.Request a) -> Cmd msg
+send wrapper request = Cmd.map wrapper <| Http.send HttpResult request
 
 pure : msg -> Cmd msg
 pure msg = Task.perform (always msg) (Task.succeed ())
