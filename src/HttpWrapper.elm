ba3d6188601987affc8c6dad0265d9b0b0002eaa
kishlaya <kishlaya.j@gmail.com>
2018-08-04 00:41:17 +0530

    Convert into proper module

--- src/HttpWrapper.elm
+++ src/HttpWrapper.elm
@@ -1,13 +1,26 @@
 module HttpWrapper
-  exposing (..)
+  exposing 
+    ( send
+    , HttpResult(..)
+    )
+
+{-| Simple wrapper over the elm HTTP module
+
+@docs send
+
+@docs HttpResult
+
+-}
 
 import Http
 import Task
 
 type alias Model = ()
 
+{-|-}
 type HttpResult a
   = HttpResult (Result Http.Error a)
 
+{-|-}
 send : ((HttpResult a) -> msg) -> (Http.Request a) -> Cmd msg
 send wrapper request = Cmd.map wrapper <| Http.send HttpResult request
